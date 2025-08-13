c549652c913434f9a7e2d17ffe71b0e17f936c66
The Gaf <bryangaffin@gmail.com>
2025-08-13 10:45:26 -0400

    Update BSettings.js

--- background/utils/BSettings.js
+++ background/utils/BSettings.js
@@ -2,30 +2,22 @@
 
 const DEFAULT_SETTINGS = {
     apiKeys: {
-        gnews: process.env.GNEWS_KEY,
-        newsapi_org: process.env.NEWSAPI_ORG_KEY,
-        newsapi_ai: process.env.NEWSAPI_AI_KEY,
-        serpApi: process.env.SERPAPI_KEY,
+        gnews: '', // Will be set in options
+        newsapi_org: '',
+        newsapi_ai: '',
+        serpApi: '',
         googleImages: {
-            apiKey: process.env.GOOGLEIMAGES_KEY,
-            cx: process.env.GOOGLEIMAGES_CX // if you want cx from env too
+            apiKey: '',
+            cx: ''
         },
-        youtube: process.env.YOUTUBE_KEY,
-        vimeo: process.env.VIMEO_KEY,
-        brave: process.env.BRAVE_KEY,
-        google_search: process.env.GOOGLE_SEARCH_KEY,
-        openai: process.env.OPENAI_KEY,
-        groq: process.env.GROQ_KEY // fixed typo from GROG_KEY
+        youtube: '',
+        vimeo: '',
+        brave: '',
+        google_search: '',
+        openai: '',
+        groq: ''
     },
-    searchConfig: {
-        newsFreshnessDays: 90,
-        maxResultsPerCategory: 50,
-        preferGoogleCSE: true,
-        usePaidImageAPIs: false,
-        minImageMegaPixels: 4,
-        requireAllTerms: true
-    }
-};
+
 
 export async function getSettings() {
     return new Promise((resolve) => {
